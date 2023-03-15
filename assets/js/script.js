567b8c4350dcc8a218c1597f6e72d0b89f9724da
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-03-15 19:10:06 +0000

    Add getStatus function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,20 @@
 // Make a GET request to the /api endpoint
 url= "https://ci-jshint.herokuapp.com/api?api_key=atJNtLjZRmDt_sSLTnEzg0134q4"
+
+const API_KEY = "atJNtLjZRmDt_sSLTnEzg0134q4";
+const API_URL = "https://ci-jshint.herokuapp.com/api"
+const resultsModal = new bootstrap.Modal(document.getElementsById("resultsModal"))
+
+document.getElementsById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+}
